91953821058ea3252dcf72f843c5e8a993474af4
Dana Varahi <dana_varahi@protonmail.com>
2020-12-02 12:08:22 +0000

    Added create route

--- server/helpers/create_router.js
+++ server/helpers/create_router.js
@@ -17,6 +17,21 @@ const createRouter = function (collection) {
     });
   });
 
+  router.post('/', (req, res) => {
+    const newItem = req.body
+    collection.insertOne(newItem)
+    .then((result) =>{
+      res.json(result.ops[0])
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500);
+      res.json({ status: 500, error: err });
+    });
+  });
+
+
+
   return router;
 }
   module.exports = createRouter;
